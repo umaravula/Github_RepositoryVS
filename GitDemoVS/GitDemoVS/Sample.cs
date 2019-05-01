f0b02b1cdf8e940358a86a9e88fd922f484cef42
uma <umasrinivasravula.vtalent@gmail.com>
2019-05-01 13:27:30 +0530

    Created new method called Add in Sample class

--- GitDemoVS/GitDemoVS/Sample.cs
+++ GitDemoVS/GitDemoVS/Sample.cs
@@ -8,5 +8,12 @@ namespace GitDemoVS
 {
     class Sample
     {
+        public int add()
+        {
+            int x = 10;
+            int y = 20;
+            int sum = x + y;
+            return sum;
+        }
     }
 }
